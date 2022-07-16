edb7f383c2c7e9da645e55939771084a9ab4f49b
高瀬英都 <>
2022-07-16 14:58:23 +0900

    draft upload

--- front/src/pages/Upload.tsx
+++ front/src/pages/Upload.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 import { ChangeEvent, useState, FC } from "react";
+import darumachanPng from '../assets/darumachan.png'
 
 export const Upload: FC = () => {
-  return <>upload</>;
+  const CenterWrapper = styled.div`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    flex-direction: column;
+  `
+  const Img = styled.img`
+    width: 80%;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+  `;
+  const Button = styled.button`
+    width: 80%;
+  `
+
+  return <CenterWrapper>
+    <Img src={darumachanPng} alt="user upload image"></Img>
+    <Button>送信する</Button>
+  </CenterWrapper>
 };
