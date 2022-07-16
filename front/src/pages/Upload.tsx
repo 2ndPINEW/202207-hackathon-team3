53e667881b2f4ca669c6c1ce514efbe2b41a1428
高瀬英都 <>
2022-07-16 15:21:31 +0900

    アップロード画面のモック完成

--- front/src/pages/Upload.tsx
+++ front/src/pages/Upload.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@chakra-ui/react";
 import styled from "@emotion/styled";
-import { ChangeEvent, useState, FC } from "react";
-import darumachanPng from '../assets/darumachan.png'
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import darumachanPng from '../assets/logo.png'
+
 
 export const Upload: FC = () => {
+  const navigate = useNavigate()
+
   const CenterWrapper = styled.div`
     display: flex;
-    height: 100%;
+    height: 80vh;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
   `
   const Img = styled.img`
@@ -17,10 +21,15 @@ export const Upload: FC = () => {
   `;
   const Button = styled.button`
     width: 80%;
+    height: 40px;
+    color: white;
+    margin-top: 32px;
+    border-radius: 100000000px;
+    background-color: #A4797D;
   `
 
   return <CenterWrapper>
     <Img src={darumachanPng} alt="user upload image"></Img>
-    <Button>送信する</Button>
+    <Button onClick={() => { navigate('gacha') }}>送信する</Button>
   </CenterWrapper>
 };
